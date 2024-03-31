f1280bfcb83695067a8ffa43f8683bca1109a929
Lee Smet <lee.smet@hotmail.com>
2024-03-31 22:03:17 +0200

    Crude image output
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let image_width = 256;
+    let image_height = 256;
+
+    println!("P3");
+    println!("{image_width} {image_height}");
+    println!("255");
+
+    for j in 0..image_height {
+        for i in 0..image_width {
+            let r = i as f64 / (image_width - 1) as f64;
+            let g = j as f64 / (image_height - 1) as f64;
+            let b = 0.;
+
+            const BASE: f64 = 255.999;
+
+            let ir = (BASE * r) as u8;
+            let ig = (BASE * g) as u8;
+            let ib = (BASE * b) as u8;
+
+            println!("{ir} {ig} {ib}");
+        }
+    }
 }
