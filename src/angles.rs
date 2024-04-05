5c2a83a05acced32dc80f6d969ea5fa917275143
Lee Smet <lee.smet@hotmail.com>
2024-04-05 20:14:53 +0200

    Generate dead code in src/angles.rs
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- src/angles.rs
+++ src/angles.rs
@@ -1,4 +1,6 @@
-// Convert degrees to the radian representation.
+#![allow(dead_code)]
+
+/// Convert degrees to the radian representation.
 fn degrees_to_radians(degrees: f64) -> f64 {
     degrees * std::f64::consts::PI / 180.0
 }
